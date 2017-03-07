0d7b083645143c557efd65b03e19e120caa58080
mcelrea <mcelrea@damien-hs.edu>
2017-03-07 09:07:31 -0800

    Added Collision Detection with map

--- core/src/com/mcelrea/platformer/GameplayScreen.java
+++ core/src/com/mcelrea/platformer/GameplayScreen.java
@@ -8,14 +8,22 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.util.Iterator;
 
 public class GameplayScreen implements Screen {
 
     private static final float WORLD_WIDTH = 800;
     private static final float WORLD_HEIGHT = 600;
+    private static final int CELL_SIZE = 40;
     private SpriteBatch batch; //draw graphics
     private ShapeRenderer shapeRenderer; //draw shapes
     private OrthographicCamera camera; //the players view of the world
@@ -32,7 +40,7 @@ public class GameplayScreen implements Screen {
     @Override
     public void show() {
         camera = new OrthographicCamera(); //2D camera
-        camera.position.set(WORLD_WIDTH/2,WORLD_HEIGHT/2,0);
+        //camera.position.set(WORLD_WIDTH/2,WORLD_HEIGHT/2,0);
         camera.update();
         viewport = new FitViewport(WORLD_WIDTH,WORLD_HEIGHT,camera);
         viewport.apply(true);
@@ -49,12 +57,17 @@ public class GameplayScreen implements Screen {
     public void render(float delta) {
         clearScreen();
 
+        player.update();
+        handlePlayerCollision();
 
+        camera.position.set(player.getX(),player.getY(),0);
+        camera.update();
 
+        mapRenderer.setView(camera);
         mapRenderer.render();
 
-        batch.setProjectionMatrix(camera.projection);
-        batch.setTransformMatrix(camera.view);
+        //batch.setProjectionMatrix(camera.projection);
+        //batch.setTransformMatrix(camera.view);
         //all graphics drawing goes here
         batch.begin();
         batch.end();
@@ -95,6 +108,94 @@ public class GameplayScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    public Array<CollisionCell> whichCellsDoesPlayerCover() {
+        float x = player.getX();
+        float y = player.getY();
+        Array<CollisionCell> cellsCovered = new Array<CollisionCell>();
+        float cellRow = x / CELL_SIZE;
+        float cellCol = y / CELL_SIZE;
+
+        int bottomLeftCellRow = MathUtils.floor(cellRow);
+        int bottomLeftCellCol = MathUtils.floor(cellCol);
+
+        TiledMapTileLayer tiledMapTileLayer = (TiledMapTileLayer)map1.getLayers().get(0);
+
+        cellsCovered.add(new CollisionCell(tiledMapTileLayer.getCell(bottomLeftCellRow,
+                bottomLeftCellCol), bottomLeftCellRow, bottomLeftCellCol));
+
+        if(cellRow % 1 != 0 && cellCol % 1 != 0) {
+            int topRightCellRow = bottomLeftCellRow + 1;
+            int topRightCellCol = bottomLeftCellCol + 1;
+            cellsCovered.add(new CollisionCell(tiledMapTileLayer.getCell(topRightCellRow,
+                    topRightCellCol), topRightCellRow, topRightCellCol));
+        }
+        if(cellRow % 1 != 0) {
+            int bottomRightCellRow = bottomLeftCellRow + 1;
+            int bottomRightCellCol = bottomLeftCellCol;
+            cellsCovered.add(new CollisionCell(tiledMapTileLayer.getCell(bottomRightCellRow,
+                    bottomRightCellCol), bottomRightCellRow, bottomRightCellCol));
+
+        }
+        if(cellCol % 1 != 0) {
+            int topLeftCellRow = bottomLeftCellRow;
+            int topLeftCellCol = bottomLeftCellCol + 1;
+            cellsCovered.add(new CollisionCell(tiledMapTileLayer.getCell(topLeftCellRow,
+                    topLeftCellCol), topLeftCellRow, topLeftCellCol));
+        }
+
+        return cellsCovered;
+    }
+
+    private Array<CollisionCell> filterOutNonCollisionCells(Array<CollisionCell> cells) {
+        for(Iterator<CollisionCell> iter = cells.iterator(); iter.hasNext();) {
+            CollisionCell collisionCell = iter.next();
+
+            if(collisionCell.isEmpty()) {
+                iter.remove();
+            }
+            else if(collisionCell.getId() == 4) {
+                iter.remove();
+            }
+        }
+
+        return cells;
+    }
+
+    public void handlePlayerCollision() {
+        Array<CollisionCell> playerCells = whichCellsDoesPlayerCover();
+        for(int i=0; i < playerCells.size; i++) {
+            System.out.print(playerCells.get(i).getId() + ", ");
+        }
+        playerCells = filterOutNonCollisionCells(playerCells);
+        for(CollisionCell cell: playerCells) {
+            float cellLevelX = cell.getCellRow() * CELL_SIZE;
+            float cellLevelY = cell.getCellCol() * CELL_SIZE;
+            Rectangle intersection = new Rectangle();
+            Intersector.intersectRectangles(player.getCollisionRectangle(),
+                    new Rectangle(cellLevelX,cellLevelY,CELL_SIZE,CELL_SIZE),
+                    intersection);
+            if(intersection.getHeight() < intersection.getWidth()) {
+
+                if(intersection.getY() == player.getY()) {
+                    player.updatePosition(player.getX(), intersection.getY() + intersection.getHeight());
+                }
+                if(intersection.getY() > player.getY()) {
+                    player.updatePosition(player.getX(), intersection.getY() - player.COLLISION_HEIGHT);
+                }
+            }
+            else if (intersection.getWidth() < intersection.getHeight()) {
+                if(intersection.getX() == player.getX()) {
+                    player.updatePosition(intersection.getX() + intersection.getWidth(),
+                            player.getY());
+                }
+                if(intersection.getX() > player.getX()) {
+                    player.updatePosition(intersection.getX() - player.COLLISION_WIDTH,
+                            player.getY());
+                }
+            }
+        }
     }
 }
 
--- core/src/com/mcelrea/platformer/Player.java
+++ core/src/com/mcelrea/platformer/Player.java
@@ -1,14 +1,23 @@
 package com.mcelrea.platformer;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Player {
-    private static final float COLLISION_WIDTH=50;
-    private static final float COLLISION_HEIGHT=70;
+    public static final float COLLISION_WIDTH=50;
+    public static final float COLLISION_HEIGHT=70;
+    private static final float MAX_X_SPEED = 2;
+    private static final float MAX_Y_SPEED = 2;
+    private static final float MAX_JUMP_DISTANCE = 3 * COLLISION_HEIGHT;
     private Rectangle collisionRectangle;
     private float x;
     private float y;
+    private float xSpeed;
+    private float ySpeed;
+    private boolean blockJump = false;
+    private float jumpYDistance = 0;
 
     public Player(float x, float y) {
         this.x = x;
@@ -18,10 +27,65 @@ public class Player {
                 COLLISION_HEIGHT);
     }
 
+    public void updatePosition(float x, float y) {
+        this.x = x;
+        this.y = y;
+        updateCollisionRectangle();
+    }
+
+    public void updateCollisionRectangle() {
+        collisionRectangle.setPosition(x,y);
+    }
+
+    public void update() {
+        Input input = Gdx.input;
+        //right
+        if(input.isKeyPressed(Input.Keys.D)) {
+            xSpeed = MAX_X_SPEED;
+        }
+        //left
+        else if(input.isKeyPressed(Input.Keys.A)) {
+            xSpeed = -MAX_X_SPEED;
+        }
+        //no x-movement
+        else {
+            xSpeed = 0;
+        }
+
+
+        //jump
+        if(input.isKeyPressed(Input.Keys.W) && !blockJump) {
+            ySpeed = MAX_Y_SPEED;
+            jumpYDistance += ySpeed;
+            blockJump = jumpYDistance > MAX_JUMP_DISTANCE;
+        }
+        //he's not jumping
+        else {
+            ySpeed = -MAX_Y_SPEED;
+            blockJump = jumpYDistance > 0;
+        }
+
+        x += xSpeed;
+        y += ySpeed;
+        updateCollisionRectangle();
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
     public void drawDebug(ShapeRenderer shapeRenderer) {
         shapeRenderer.rect(collisionRectangle.x,
                 collisionRectangle.y,
                 collisionRectangle.width,
                 collisionRectangle.height);
+    }
+
+    public Rectangle getCollisionRectangle() {
+        return collisionRectangle;
     }
 }
